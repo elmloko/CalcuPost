3f9c35ea9a7d6b610d45f705cf032ef6987c0a71
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-07-04 14:59:14 -0400

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-inline">
         {{ date('Y') }} Agencia Boliviana de Correos ©
-        <strong>CalcuPost</strong> Todos los derechos reservados.
+        <strong>POSTAR</strong> Todos los derechos reservados.
     </div>
-    Todos los derechos reservados.<strong>POSTAR</strong>
+    Todos los derechos reservados.<strong>CalcuPost</strong>
     <a href="mailto:mespinozarojas46@gmail.com" class="opacity-75" title="Marco Antonio Espinoza Rojas">Copyright © MAER
         {{ date('Y') }} </a>
 </footer>
